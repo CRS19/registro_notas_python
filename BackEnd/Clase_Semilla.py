a5edb92316148270a0550a379d751c7609722578
Artins <adjimenez6@espe.edu.ec>
2022-01-04 23:03:52 -0500

    Clase Semilla Añadido

---
+++ BackEnd/Clase_Semilla.py
@@ -0,0 +1,25 @@
+from typing import SupportsFloat
+
+
+class Estudiante:
+
+    def __init__ (self, id, cedula, nombre_apellido, registro):
+        self.id = id
+        self.cedula = cedula
+        self.nombre_apellido = nombre_apellido
+        self.registro = registro
+    
+    def asistir(self,asistencia):
+        
+        #asistencia.upper()
+        #if asistencia == "SI":
+            
+        #else:
+
+        print(f"El estudiante {asistencia} asistio")
+
+
+    def nueva_asistencia(self):
+        self.asistencia.append()
+    
+             
